3d82b25e62f8d0033f5fa802f96430c1ab6afe87
Aibashkin <167601651+Aibashkin@users.noreply.github.com>
2024-05-04 08:40:43 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,14 +1,83 @@
 const burger = () => {
-    const btn = document.querySelector('.burger svg:last-child')
+    const btn = document.querySelector('#burger')
     const off = document.querySelector('.offset')
     const close = document.querySelector('.offset nav svg')
+    let currentImageIndex = 0;
     btn.addEventListener('click', () => {
         off.style.display = 'block'
     })
     close.addEventListener('click', () => {
         off.style.display = 'none'
-        
     })
 } 
+
+
+
+
+
+const sliderImage = document.querySelector("header footer div img");
+const leftButton = document.querySelector("header footer #left");
+const rightButton = document.querySelector("header footer #right");
+const sliderText = document.querySelector("header footer span");
+
+
+let currentImageIndex = 0;
+const images = ["images/slider1.jpg", "images/slider2.jpg", "images/slider3.jpg"];
+const texts = ["Популярные бренды энергетических напитков на нашем сайте",
+ "Заказывайте любимые энергетики через удобный вам маркетплейс",
+ "Cвежие новости в мире энергетических напитков у нас на сайте"];
+
+function changeImage(direction) {
+  if (direction === "left") {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+  } else if (direction === "right") {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+  }
+
+  sliderImage.src = images[currentImageIndex];
+  sliderText.textContent = texts[currentImageIndex];
+}
+
+leftButton.addEventListener("click", () => changeImage("left"));
+rightButton.addEventListener("click", () => changeImage("right"));
+
+
+
+
+
+
+
+
+
+
+
+// const slider = () => {
+//     const sliderImage = document.querySelectorAll('header footer div img')
+//     const btnLeft = document.querySelectorAll('header footer #left')
+//     const btnRight = document.querySelectorAll('header footer #right')
+//     console.log(sliderImage, btnLeft, btnRight)
+
+//     function removeActive(array){
+//         for (const elem of array) {
+//            elem.classList.remove('active')
+//         }
+//      }
+
+//     btnLeft.addEventListener('click', () => {
+//         removeActive(sliderImage)
+//     })
+// }
+
+// slider()
+
+
+
+
+
+
+
+
+
+
 
 burger()
